ee6f2ea19bfe381378af77adc8b5fbb71e2a38fc
Bateristico <camilo.saa@gmail.com>
2020-12-09 22:38:09 +0100

    8. Navbar creation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 //Import Pages
 import AboutUs from './pages/AboutUs';
+import Nav from './components/Nav';
 //Global Style
 import GlobalStyle  from './components/GlobalStyle';
+
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
