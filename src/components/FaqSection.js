3b98b8942f34c26de08f9e8c639c08411bf112c9
Bateristico <camilo.saa@gmail.com>
2020-12-16 19:21:54 +0100

    17. Reusable components

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 import { About } from '../styles';
+import Toggle from './Toggle';
 
 const FaqSection = () => {
     return (
         <Faq>
             <h2>Any Questions? <span>FAQ</span></h2>
-            <div className="question">
-                <h4>How do I start?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, soluta!</p>
+            <Toggle>
+                <div className="question">
+                    <h4>How do I start?</h4>
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, tenetur harum vel commodi nemo nisi eaque laudantium delectus iusto fugiat?</p>
+                    </div>
+                    <div className="faq-line"></div>
                 </div>
-                <div className="faq-line"></div>
-            </div>
+            </Toggle>
             <div className="question">
                 <h4>Daily schedule</h4>
                 <div className="answer">
                     <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, soluta!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis culpa odit ullam ducimus eum aut eligendi excepturi, aliquid quas deleniti.</p>
                 </div>
                 <div className="faq-line"></div>
             </div>
@@ -26,7 +29,7 @@ const FaqSection = () => {
                 <h4>Different Payment methods</h4>
                 <div className="answer">
                     <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, soluta!</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi quibusdam cum assumenda veniam eos. Quia asperiores molestias provident nesciunt reprehenderit.</p>
                 </div>
                 <div className="faq-line"></div>
             </div>
@@ -34,7 +37,7 @@ const FaqSection = () => {
                 <h4>What products do you offer</h4>
                 <div className="answer">
                     <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, soluta!</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum repellat quaerat nostrum est quibusdam tempore iure aperiam dolor iusto consequatur.</p>
                 </div>
                 <div className="faq-line"></div>
             </div>
