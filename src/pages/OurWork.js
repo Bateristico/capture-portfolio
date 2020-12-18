bc44691ae126e35d6dd20ba180ed13daab9c2aaf
Bateristico <camilo.saa@gmail.com>
2020-12-18 13:46:00 +0100

    20. Our Work Scroll

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -10,9 +10,14 @@ import goodtimes from "../img/goodtimes-small.png";
 //Animations
 import { motion } from 'framer-motion';
 import { sliderContainer, slider, pageAnimation, fade, photoAnim, lineAnim } from '../animation';
+import { useScroll } from '../components/useScroll';
 
 
 const OurWork = () => {
+
+    const [element, controls] = useScroll();
+    const [element2, controls2] = useScroll();
+
     return(
         <Work exit="exit" variants={pageAnimation} initial="hidden" animate="show" style={{background: "#FFF" }}> 
             <motion.div variants={sliderContainer}>
@@ -30,16 +35,16 @@ const OurWork = () => {
                     </Hide>
                 </Link>
             </Movie>
-            <Movie>
+            <Movie ref={element} variants={fade} animate={controls} initial="hidden"> 
                 <h2>The Racer</h2>
-                <div className="line"></div>
+                <motion.div variants={lineAnim} className="line"></motion.div>
                 <Link to="/work/the-racer">
                     <img src={theracer} alt="theracer"/>
                 </Link>
             </Movie>
-            <Movie>
+            <Movie ref={element2} variants={fade} animate={controls2} initial="hidden">
                 <h2>Good Times</h2>
-                <div className="line"></div>
+                <motion.div variants={lineAnim} className="line"></motion.div>
                 <Link to="/work/good-times">
                     <img src={goodtimes} alt="goodtimes"/>
                 </Link>
@@ -59,7 +64,7 @@ const Work = styled(motion.div)`
     }
 `;
 
-const Movie = styled.div`
+const Movie = styled(motion.div)`
     padding-bottom: 10rem;
     .line {
         height: 0.5rem;
